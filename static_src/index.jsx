ec5fa9cb3ce415dc221e7951b2516dcd7d61500c
YuliaAnikeeva <yulia-anikeeva@yandex.ru>
2019-12-03 15:09:43 +0300

    6.1 done

--- static_src/index.jsx
+++ static_src/index.jsx
@@ -5,12 +5,21 @@ import ReactDOM from 'react-dom';
 let messages = ['Привет', 'Как дела?'];
 
 const MessageComponent = (props) => <div>{props.text}</div>;
-
+const MessageButton = (props) => <button onClick={handleClick}>Norm</button>;
 const MessageField = (props) => {
    return props.messages.map(message => <MessageComponent text={ message } />);
 };
 
 ReactDOM.render(
-   <MessageField messages={ messages } />,
+   <div>
+      <MessageField messages={ messages } />
+      <MessageButton/>
+   </div>,
    document.getElementById('root'),
 );
+const handleClick = () => {
+   
+   messages.push('Norm');
+   ReactDOM.render();
+   };
+   
