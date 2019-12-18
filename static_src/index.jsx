b1a6afd8ab199bc8d2e3d2f2cd922150fc6c4aaa
YuliaAnikeeva <yulia-anikeeva@yandex.ru>
2019-12-18 12:32:17 +0300

    hw 1-2

--- static_src/index.jsx
+++ static_src/index.jsx
@@ -1,31 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/MessageField.jsx';
-
-// let messages = ['Привет', 'Как дела?'];
-
-// const handleClick = () => {
-//     messages.push('Нормально');
-//     ReactDOM.render(
-//         <MessageField messages={ messages } />,
-//         document.getElementById('root'),
-//     );
-// };
-
-// const MessageComponent = (props) => <div>{ props.text }</div>;
-
-// const MessageField = (props) => {
-//    const messageElemnts = props.messages.map(message => <MessageComponent text={ message } />);
-//    return (
-//        <div>
-//            <h1>React Chat</h1>
-//            { messageElemnts }
-//            <button onClick={ handleClick }>Отправить сообщение</button>
-//        </div>
-//    )
-// };
+import MessageField from './components/MessageField'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 ReactDOM.render(
-   <App />,
+   <MuiThemeProvider>
+       <MessageField />
+   </MuiThemeProvider>,
    document.getElementById('root'),
 );
