19afc751f38f1965f376667115ec8735cfcb9e73
YuliaAnikeeva <yulia-anikeeva@yandex.ru>
2019-12-18 08:52:50 +0300

    sender

--- static_src/components/MessageField.jsx
+++ static_src/components/MessageField.jsx
@@ -1,33 +1,38 @@
 import React from 'react';
 import Message from './Message';
+import '../styles/styles.css';
 
 export default class MessageField extends React.Component {
-   state = {
-       messages: ["Привет!", "Как дела?"],
-       author: ["Tim"]
-   };
-   componentDidUpdate() {
-    if (this.state.messages.length % 3 === 1) {  // Остаток от деления на 2
-        setTimeout(() =>
-        this.setState(
-            { messages: [ ...this.state.messages, 'Не приставай ко мне, я робот!' ] }),
-        1000);
+    state = {
+        messages: [{ text: "Привет!", sender: "bot" }, { text: "Как дела?", sender: "bot" }],
+    };
+ 
+    componentDidUpdate() {
+        if (this.state.messages[this.state.messages.length - 1].sender === "me") {
+            setTimeout(() =>
+                    this.setState({
+                        messages: [ ...this.state.messages, {text: 'Не приставай ко мне, я робот!', sender: "bot"} ] }),
+                1000);
+        }
     }
-}
-
-   handleClick = () => {
-       this.setState({ messages: [ ...this.state.messages, 'Нормально', this.state.author ] });
-   };
-   
-   render() {
-       const messageElements = this.state.messages.map((text, index) => (
-           <Message key={ index } text={ text } />));
-        
-
-       return <div>
+ 
+    handleClick = () => {
+        this.setState({ messages: [ ...this.state.messages, {text: 'Нормально', sender: "me"} ] });
+    };
+ 
+    render() {
+        const messageElements = this.state.messages.map((message, index) => (
+            <Message key={ index } text={ message.text } />));
+ 
+            return <div className="layout">
+       <div className="message-field">
            { messageElements }
-           
-           <button onClick={ this.handleClick }>Отправить сообщение</button>
        </div>
-   }
-}
+       <button onClick={ this.handleClick }>Отправить сообщение</button>
+    </div>
+
+
+     
+    }
+ }
+ 
